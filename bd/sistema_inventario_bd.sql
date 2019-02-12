5c561e946e3b79ef2e51430dd7e6941f188d2ece
ToniR95 <44881409+ToniR95@users.noreply.github.com>
2019-02-11 19:33:17 -0600

    bd remplazado

--- bd/sistema_inventario_bd.sql
+++ bd/sistema_inventario_bd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-02-2019 a las 02:26:26
+-- Tiempo de generación: 12-02-2019 a las 02:32:39
 -- Versión del servidor: 10.1.36-MariaDB
 -- Versión de PHP: 7.2.11
 

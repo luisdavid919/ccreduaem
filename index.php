bf7e9d76cf5fcd4ba54bcc7551191a5ad5044831
ToniR95 <44881409+ToniR95@users.noreply.github.com>
2019-02-13 17:50:30 -0600

    Formulario Actualizado P2

--- index.php
+++ index.php
@@ -1,4 +1,93 @@
-<!doctype html>
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: admin.php");
+    exit;
+}
+
+// Include config file
+require_once "config.php";
+
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = "";
+
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    // Check if username is empty
+    if(empty(trim($_POST["usuario"]))){
+        $username_err = "Porfavor ingresa el usuario.";
+    } else{
+        $username = trim($_POST["usuario"]);
+    }
+
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Por favor ingresa el password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT id, usuario, password FROM login WHERE usuario = ?";
+
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+
+            // Set parameters
+            $param_username = $username;
+
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+
+                // Check if username exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            // Password is correct, so start a new session
+                            session_start();
+
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["usuario"] = $username;
+
+                            // Redirect user to welcome page
+                            header("location: admin.php");
+                        } else{
+                            // Display an error message if password is not valid
+                            $password_err = "El password es inválido";
+                        }
+                    }
+                } else{
+                    // Display an error message if username doesn't exist
+                    $username_err = "No existe cuenta relacionada";
+                }
+            } else{
+                echo "Oops! Algo falló";
+            }
+        }
+
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+
+    // Close connection
+    mysqli_close($link);
+}
+?>
+<!DOCTYPE html>
 <html lang="es">
 <head>
   <meta charset="utf-8">
@@ -29,12 +118,11 @@
   <div class="row justify-content-center">
     <div class="col col-xs-12 col-sm-12 col-md-12 col-lg-8 d-block d-sm-block d-md-block mt-5">
         <h2>Iniciar Sesión</h2>
-        <p class="form-text text-center"><strong>Por favor Ingrese el Usuario y la Contraseña que se le asignó.</strong></p>
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
           <div class="form-group row justify-content-center <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
             <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 text-center mt-2">
               <label><strong>Usuario:</strong></label>
-              <input type="text" name="usuario" class="form-control" placeholder="Usario" required value="<?php echo $username; ?>">
+              <input type="text" name="usuario" class="form-control" placeholder="Usuario" required value="<?php echo $username; ?>">
               <span class="help-block"><?php echo $username_err; ?></span>
             </div>
           </div>
@@ -51,6 +139,7 @@
               </div>
             </div>
       </form>
+      <p class="form-text text-center"><strong>Por favor Ingrese el Usuario y la Contraseña que se le asignó.</strong></p>
     </div>
   </div>
 </div>
